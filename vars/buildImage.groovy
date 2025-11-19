9b59d5538da64c9656312c6bcdac074e1fbd529f
Nagornyi <artnagornyi@gmail.com>
2025-11-19 08:43:47 +0300

    add params

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,14 +1,14 @@
 #!/user/bin/env groovy
 
 //import com.example.Docker
-//def call(String imageName) {
+def call(String imageName) {
 //    return new Docker(this).buildDockerImage(imageName)
 //}
-def call() {
+//def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t artnagornyi/demo-app:jma-2.0 ."
+        sh "docker build -t $imageName ."
         sh 'echo $PASS | docker login -u $USER --password-stdin'
-     sh "docker push artnagornyi/demo-app:jma-2.0"
+     sh "docker push $imageName"
     }
 }
--- vars/buildJar.groovy
+++ vars/buildJar.groovy
@@ -1,7 +1,6 @@
 #!/user/bin/env groovy
 
 def call() {
-//    echo "building the application for branch $GIT_BRANCH"
-    echo "building the application ..."
+    echo "building the application for branch $GIT_BRANCH"
     sh 'mvn clean package'
 }
